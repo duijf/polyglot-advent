08af4d38173beec1bee95ce2adbeace78c96158e
Laurens Duijvesteijn <git@duijf.io>
2020-12-11 21:31:59 +0100

    01: Clean up hello.asm

--- 01-assembly/hello.asm
+++ 01-assembly/hello.asm
@@ -1,14 +1,21 @@
-          global      _start
+%define SYS_EXIT 60
+%define SYS_WRITE 1
+%define STDOUT 1
+%define EXIT_SUCCESS 0
 
-          section     .text
-_start:   mov         rax, 1          ; System call for write.
-          mov         rdi, 1          ; File handle 1: stdout.
-          mov         rsi, message    ; Address of string to output.
-          mov         rdx, 13         ; Number of bytes.
-          syscall                     ; Invoke OS.
-          mov         rax, 60         ; System call for exit.
-          xor         rdi, rdi        ; Exit code 0.
-          syscall                     ; Invoke OS.
+global _start
 
-          section     .data
-message:  db          "Hello, world", 10 ; Note the newline at the end
+section .text
+_start:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, message
+    mov rdx, 13
+    syscall
+
+    mov rax, SYS_EXIT
+    mov rdi, EXIT_SUCCESS
+    syscall
+
+section .data
+message db "Hello, world", 10
