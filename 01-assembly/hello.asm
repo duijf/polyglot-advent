520d1e26558083bbbe85924c51b7437b72bb9fff
Laurens Duijvesteijn <git@duijf.io>
2020-12-11 11:42:31 +0100

    01: Deduplicate hello world code

--- 01-assembly/hello.asm
+++ 01-assembly/hello.asm
@@ -1,14 +1,14 @@
-          global    _start
+          global      _start
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+          section     .text
+_start:   mov         rax, 1          ; System call for write.
+          mov         rdi, 1          ; File handle 1: stdout.
+          mov         rsi, message    ; Address of string to output.
+          mov         rdx, 13         ; Number of bytes.
+          syscall                     ; Invoke OS.
+          mov         rax, 60         ; System call for exit.
+          xor         rdi, rdi        ; Exit code 0.
+          syscall                     ; Invoke OS.
 
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+          section     .data
+message:  db          "Hello, world", 10 ; Note the newline at the end
