30a0e7531046c20d574fd817b04f91bf8fc9ca59
Laurens Duijvesteijn <git@duijf.io>
2020-12-11 21:53:45 +0100

    01: Extract a print utility from hello.asm

--- 01-assembly/hello.asm
+++ 01-assembly/hello.asm
@@ -4,15 +4,12 @@ global _start
 
 section .text
 _start:
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, message
-    mov rdx, 13
-    syscall
+    mov rax, message
+    call print
 
     mov rax, SYS_EXIT
     mov rdi, EXIT_SUCCESS
     syscall
 
 section .data
-message db "Hello, world", 10
+message db "Hello, world", 10, 0
--- 01-assembly/linux64.asm
+++ 01-assembly/linux64.asm
@@ -11,3 +11,27 @@
 %define EXIT_FAILURE 1
 
 %define O_RDONLY 0
+
+;; Write bytes to stdout.
+;;
+;; `rax` - Pointer to the bytes to write to stdout. This routine
+;; starts a search at `[rax]` until it finds a NUL byte. It then
+;; prints all data until that NUL byte.
+print:
+    push rax
+    mov rbx, 0
+;; Loop until finding a NUL byte.
+printLoop:
+    inc rax
+    inc rbx
+    ;; Move the lower byte of the value at `[rax]`
+    mov cl, [rax]
+    cmp cl, 0
+    jne printLoop
+
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    pop rsi
+    mov rdx, rbx
+    syscall
+    ret
