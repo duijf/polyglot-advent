d4316ef57499911a88a18d2e8a08264d813b4d10
Laurens Duijvesteijn <git@duijf.io>
2020-12-16 19:28:38 +0100

    04: Read lines of the input file

--- 04-java/solution-4a.java
+++ 04-java/solution-4a.java
@@ -1,5 +1,12 @@
+import java.io.*;
+import java.nio.*;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.util.List;
+
 class Solution {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Path input = FileSystems.getDefault().getPath("input");
+        List<String> lines = Files.readAllLines(input, StandardCharsets.UTF_8);
     }
 }
