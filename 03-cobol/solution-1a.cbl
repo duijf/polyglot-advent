e3fe0760b889f613a30d7a0471c685b6fe99e9dc
Laurens Duijvesteijn <git@duijf.io>
2020-12-16 17:58:41 +0100

    03: Finish solution of first part

--- 03-cobol/solution-1a.cbl
+++ 03-cobol/solution-1a.cbl
@@ -17,6 +17,11 @@
        WORKING-STORAGE SECTION.
        01 ws-line PIC A(31).
        01 ws-eof PIC A VALUE 'N'.
+       01 ws-line-count PIC 9999 VALUE ZEROES.
+       01 ws-tree-count PIC 9999 VALUE ZEROES.
+       01 ws-index PIC 9999 VALUE 1.
+      * Variable for scratch calculations. Not used for control flow.
+       01 ws-scratch PIC 9999 VALUE ZEROES.
 
        PROCEDURE DIVISION.
        para-main.
@@ -29,11 +34,21 @@
                END-READ
            END-PERFORM
 
+           DISPLAY ws-tree-count
+
            CLOSE fd-input
 
            STOP RUN
            .
 
        para-line.
-           DISPLAY ws-line
+           MULTIPLY ws-line-count BY 3 GIVING ws-scratch
+           DIVIDE ws-scratch BY 31 GIVING ws-scratch REMAINDER ws-index
+           ADD 1 TO ws-index GIVING ws-index
+
+           IF ws-line(ws-index:1) = '#'
+              ADD 1 TO ws-tree-count GIVING ws-tree-count
+           END-IF
+
+           ADD 1 TO ws-line-count GIVING ws-line-count
            .
