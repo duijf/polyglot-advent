8fd05821969393aff3716caf8966439442656653
Laurens Duijvesteijn <git@duijf.io>
2020-12-16 19:17:46 +0100

    04: Add a Java hello world
    I'm aware that you can go completely nuts with build systems and
    editors in Javaland. Let's not go there and keep it simple.

---
+++ 04-java/README.md
@@ -0,0 +1,10 @@
+# Challenge 4 - Java
+
+## Running
+
+```
+# Get the development tools.
+$ nix-shell
+
+$ ./run.sh solution-04.java
+```
---
+++ 04-java/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Run a single Java file as an executable.
+#
+# EXAMPLE
+#
+#     $ nix-shell
+#     $ ./run.sh solution-4a.java
+#
+# ENVIRONMENT
+#
+# Run this from the shell defined by `shell.nix`. E.g. `nix-shell`.
+# (Or install the development tools in some other way).
+
+set -eufo pipefail
+
+# The first argument (`$1`) should contain the filename of the assembly
+# file to build.
+if [ -z ${1+x} ]; then
+    echo "usage: $0 JAVA_FILE"
+    exit 1
+else
+    JAVA_FILE="$1"
+    BASENAME="$(basename "$JAVA_FILE" .java)"
+fi
+
+# Run it.
+java "$JAVA_FILE"
