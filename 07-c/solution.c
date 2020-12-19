abca225326cf7d4f18d709454baa6dba8c2bbe96
Laurens Duijvesteijn <git@duijf.io>
2020-12-19 21:19:27 +0100

    07: Read input file into memory

--- 07-c/solution.c
+++ 07-c/solution.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    printf("Hello world!");
+    FILE *f = fopen("input", "rb");
+
+    // Get the file size by seeking to the end, asking for the file
+    // position and resetting to the beginning.
+    fseek(f, 0, SEEK_END);
+    long file_size = ftell(f);
+    rewind(f);
+
+    // Create a space for the contents of the file on the heap. Leave
+    // room for the final string termination character (`\0`).
+    char *contents = malloc(file_size + 1);
+    size_t read_size = 1;
+    size_t _bytes_read = fread(contents, read_size, file_size, f);
+
+    fclose(f);
+    contents[file_size] = 0;
+
+    puts(contents);
+
     return 0;
 }
